122e8b7f55ad2189ba9d4f888c224239d81577b5
rhard <rhard@mail.ru>
2018-03-15 08:16:25 +0100

    Running prototype

--- conanfile.py
+++ conanfile.py
@@ -1,42 +1,34 @@
 from conans import ConanFile, CMake, tools
-
+import os
 
 class ImguiConan(ConanFile):
     name = "imgui"
     version = "1.53"
-    license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Imgui here>"
+    license = "https://github.com/ocornut/imgui/blob/master/LICENSE.txt"
+    url = "https://github.com/rhard/conan-imgui"
+    description = "Dear ImGui is a bloat-free graphical user interface library for C++."
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
+    exports_sources = "CMakeLists.txt"
 
     def source(self):
-        self.run("git clone https://github.com/memsharded/hello.git")
-        self.run("cd hello && git checkout static_shared")
-        # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
-        # if the packaged project doesn't have variables to set it properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)", '''PROJECT(MyHello)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        print(self.source_folder)
+        if not os.path.exists(os.path.join(self.source_folder, "imgui")):
+            self.run("git clone https://github.com/ocornut/imgui.git")
+        self.run("cd imgui && git fetch --all --tags")
+        self.run("cd imgui && git checkout tags/v%s" % self.version)
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="hello")
+        cmake.configure()
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s' % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("imgui.h", dst="include", src="imgui")
+        self.copy("imgui_internal.h", dst="include", src="imgui")
+        self.copy("imgui.lib", dst="lib", src="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["imgui"]
