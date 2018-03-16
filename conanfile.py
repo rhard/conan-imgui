c7d033252b065634eece859696193a7c4273ffcb
Roman <rhard.x.plane@gmail.com>
2018-03-16 23:45:17 +0100

    Fix for Windows build

--- conanfile.py
+++ conanfile.py
@@ -36,7 +36,7 @@ class ImguiConan(ConanFile):
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("imgui.lib", dst="lib", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["imgui"]
